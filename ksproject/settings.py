9dbd747895b81c090ec8a5765af0fe597d13ba49
manato0713 <utm2577215@stu.o-hara.ac.jp>
2025-11-28 01:11:24 +0900

    Update settings.py

--- ksproject/settings.py
+++ ksproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-^xdo_x30t)%6v&ack=i-5f*xjzf2x^x5zvmmpxdm^y@$&1a181
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["manato0713.pythonanywhere.com"]
+ALLOWED_HOSTS = ['manato0713.pythonanywhere.com']
 
 
 # Application definition
